612a6f55c9b2ce927dafd5e1e3ab4424644582cd
unknown <jair.lopes@fatec.sp.gov.br>
2021-09-19 22:33:42 -0300

    Event binding

---
+++ DATA-BINDING/Aula-10-Event Binding/data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  isMouseOver: boolean = false;
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem: string = 'http://lorempixel.com/400/200/nature/';
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  botaoClicado() {
+    alert("Botão clicado!");
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>event.target).value;
+  }
+
+  salvarValor(event: string) {
+    this.valorSalvo = event;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
