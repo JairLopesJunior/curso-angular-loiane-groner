25335a7319a6394e10e84de290d82f8716170e9d
JairLopesJr <jair.lopes@vrsoft.com.br>
2021-09-29 10:07:00 -0300

    Ciclo de vida (life-cycle) do Componente

---
+++ DATA-BINDING/Aula-14-Ciclo-de-vida (life-cycle) do Componente/data-binding/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertiesComponent } from './output-properties/output-properties.component';
+import { CicloComponent } from './ciclo/ciclo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBindingComponent,
+    InputPropertyComponent,
+    OutputPropertiesComponent,
+    CicloComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
