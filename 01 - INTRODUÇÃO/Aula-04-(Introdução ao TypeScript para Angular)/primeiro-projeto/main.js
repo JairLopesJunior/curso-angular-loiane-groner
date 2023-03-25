22bae3d60ca799ce8c6ca79803730068ffe33802
unknown <jair.lopes@fatec.sp.gov.br>
2023-03-25 19:21:26 -0300

    Introdução ao TypeScript para Angular

---
+++ 01 - INTRODUÇÃO/Aula-04-(Introdução ao TypeScript para Angular)/primeiro-projeto/main.js
@@ -0,0 +1,6 @@
+var minhaVar = 'Minha Variavel';
+function minhaFunction(x, y) {
+    return x + y;
+}
+var num = 2;
+console.log(minhaVar);
---
+++ 01 - INTRODUÇÃO/Aula-04-(Introdução ao TypeScript para Angular)/primeiro-projeto/main.ts
@@ -0,0 +1,19 @@
+var minhaVar = 'Minha Variavel';
+
+function minhaFunction(x, y) {
+    return x + y;
+}
+
+// ES 6 ou ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(n => n * 2);
+
+class Matematica {
+    soma(x, y) {
+        return x + y;
+    }
+}
+
