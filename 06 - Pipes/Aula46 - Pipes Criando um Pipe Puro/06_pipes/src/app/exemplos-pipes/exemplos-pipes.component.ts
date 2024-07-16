555a9eeab2222c452bd3e2cc9b86e44bea918ea2
unknown <jair.lopes@fatec.sp.gov.br>
2024-07-16 16:12:27 -0300

    Pipes: Criando um Pipe Puro

---
+++ 06 - Pipes/Aula46 - Pipes Criando um Pipe Puro/06_pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent implements OnInit {
+
+  livro: any = {
+    titulo: 'Learning JavaScript',
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co.gsdfsvioc'
+  }
+
+  livros: string[] = ['Java', 'Angular 2'];
+
+  filtro: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  addCurso(value: string): void {
+    this.livros.push(value);
+  }
+}
