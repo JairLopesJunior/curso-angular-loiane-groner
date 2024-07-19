9d3535bb8593609589acc452b1fd3c8823c6205b
unknown <jair.lopes@fatec.sp.gov.br>
2024-07-19 17:30:27 -0300

    Forms (template driven) Populando campos com setValue e patchValue (CEP)

---
+++ 09 - Formularios/TEMPLATE DRIVEN/Aula86 - Forms template driven Populando campos com setValue e patchValue CEP/forms/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
+import { FormsModule } from '@angular/forms';
+import { TemplateFormModule } from './template-form/template-form.module';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    TemplateFormModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 09 - Formularios/TEMPLATE DRIVEN/Aula86 - Forms template driven Populando campos com setValue e patchValue CEP/forms/src/app/template-form/template-form.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  usuario: any = {
+    nome: 'jair',
+    email: 'jair@gmail.com'
+  }
+
+  constructor(private _http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  aplicaCssErro(campo: any): any {
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+     }
+  }
+
+  verificaValidTouched(campo: any): boolean {
+    return !campo.valid && campo.touched;
+  }
+
+  onSubmit(form: any): void {
+    console.log(form);
+  }
+
+  consultaCEP(cep: string, form: any): void {
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != "") {
+      var validaCep = /^[0-9]{8}$/;
+
+      if (validaCep.test(cep)) {
+        this._http.get(`//viacep.com.br/ws/${cep}/json`)
+        .subscribe((dados: any) => {
+          this.populaDadosForm(dados, form);
+        });
+      }
+    }
+  }
+
+  populaDadosForm(dados: any, form: any): void {
+    form.setValue({
+      nome: form.value.nome,
+      email: form.value.email,
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        numero: '',
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+    
+    /*form.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        numero: '',
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });*/
+  }
+}
