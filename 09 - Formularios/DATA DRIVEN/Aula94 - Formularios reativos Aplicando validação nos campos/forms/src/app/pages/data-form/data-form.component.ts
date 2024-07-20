7c260c102b365dff86bf58d48d97a2e80e851ba4
unknown <jair.lopes@fatec.sp.gov.br>
2024-07-20 17:56:58 -0300

    Formularios reativos Aplicando validacao nos campos

---
+++ 09 - Formularios/DATA DRIVEN/Aula94 - Formularios reativos Aplicando validação nos campos/forms/src/app/pages/data-form/data-form.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-form',
+  templateUrl: './data-form.component.html',
+  styleUrls: ['./data-form.component.css']
+})
+export class DataFormComponent implements OnInit {
+
+  formulario!: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder,
+              private _httpClient: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    /*this.formulario = new FormGroup({
+      nome: new FormGroup(null),
+      email: new FormGroup(null)
+    });*/
+
+    this.formulario = this._formBuilder.group({
+      nome: [null, Validators.required],
+      email: [null, [
+        Validators.required,
+        Validators.email
+      ]]
+    });
+  }
+
+  onSubmit(): void {
+    this._httpClient.post('enderecaoServer/formUsuario', this.formulario.getRawValue()).subscribe((dados: any) => {
+      this.reset();
+    },
+    (error: any) => alert('erro'));
+  }
+
+  reset(): void {
+    this.formulario.reset();
+  }
+}
