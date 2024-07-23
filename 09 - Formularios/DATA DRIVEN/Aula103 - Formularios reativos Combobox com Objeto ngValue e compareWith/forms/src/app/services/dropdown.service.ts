354f366c712e20a9ea7e0dd383a2a437a95f24d4
unknown <jair.lopes@fatec.sp.gov.br>
2024-07-23 10:29:53 -0300

    Formulários reativos: Combobox com Objeto (ngValue e compareWith)

---
+++ 09 - Formularios/DATA DRIVEN/Aula103 - Formularios reativos Combobox com Objeto ngValue e compareWith/forms/src/app/services/dropdown.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DropdownService {
+
+  constructor() { }
+
+  getEstadosBr(): any {
+    return [
+      {
+        "id": 1,
+        "sigla": "AC",
+        "nome": "Acre"
+      },
+      {
+        "id": 2,
+        "sigla": "AL",
+        "nome": "Alagoas"
+      }
+    ];
+  }
+
+  getCargos(): any {
+    return [
+      { nome: 'Dev', nivel: 'Junior', desc: 'Dev Jr' },
+      { nome: 'Dev', nivel: 'Pleno', desc: 'Dev Pl' },
+      { nome: 'Dev', nivel: 'Senior', desc: 'Dev Sr' }
+    ];
+  }
+}
